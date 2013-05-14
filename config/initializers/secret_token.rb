221ce321c53b922a0a20b559ceea3bd85cc09ddf
Vinay <vinay59.grewal@gmail.com>
2013-05-15 00:46:41 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MerlinVertis::Application.config.secret_token = 'cfad5f47aa3c007e27488a80198909d083845de53e18cf5af8658698cd27960112763bf8a440fcd2b62b03a05f8d049cf0929724b59ce9f09f58084dddf262bd'
